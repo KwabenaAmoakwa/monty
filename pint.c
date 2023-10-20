98e06c12759b2b2e0b10bb04fbaa4a09ff69e51b
Jewelz_B <jewelbansah@rocketmail.com>
2023-10-20 01:30:24 +0000

    fix pint

--- pint.c
+++ pint.c
@@ -10,7 +10,7 @@ void pint(stack_t **head, unsigned int line_number)
 	if (*head == NULL)
 	{
 		printf("L%d: can't pint, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
 	}
-
 	printf("%d\n", (*head)->n);
 }
--- pop.c
+++ pop.c
@@ -12,6 +12,7 @@ void pop(stack_t **head, unsigned int line_number)
 	if (*head == NULL)
 	{
 		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
+		free_stack(*head);
 		exit(EXIT_FAILURE);
 	}
 	temp = *head;
