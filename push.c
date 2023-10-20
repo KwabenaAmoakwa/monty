61c48b2caec3d1c5c288882522b80edcc542a32e
KwabenaAmoakwa <amoakwakwabena@gmail.com>
2023-10-19 17:14:05 -0700

    monty

--- push.c
+++ push.c
@@ -36,21 +36,3 @@ void push(stack_t **head, unsigned int line)
 		(*head) = newnode;
 	}
 }
-/**
- * pall - prints the stack
- * @head: stack head
- * @line_number: no used
- * Return: no return
-*/
-void pall(stack_t **head, unsigned int line_number)
-{
-	stack_t *temp;
-	(void)line_number;
-
-	temp = *head;
-	while (temp)
-	{
-		printf("%d\n", temp->n);
-		temp = temp->next;
-	}
-}
