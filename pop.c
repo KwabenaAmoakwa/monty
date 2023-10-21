7088045645f95597963f1551d2ce62093bfc9e06
KwabenaAmoakwa <amoakwakwabena@gmail.com>
2023-10-21 04:58:17 -0700

    pop

--- pop.c
+++ pop.c
@@ -11,7 +11,7 @@ void pop(stack_t **head, unsigned int line_number)
 
 	if (*head == NULL)
 	{
-		fprintf(stderr, "L%d:can't pop an empty stack\n", line_number);
+		fprintf(stderr, " L%d:can't pop an empty stack\n", line_number);
 		free_stack(*head);
 		exit(EXIT_FAILURE);
 	}
